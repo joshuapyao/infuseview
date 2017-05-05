7a17ddb905b6adcf1008fcad4ad4f9872a5d1f90
Paulina Tsai <paulinatsai@Paulinas-MacBook-Pro.local>
2017-05-05 12:46:46 -0700

    module template populated by js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 window.onload = function() {
-	alert("hi");
+	loadModules();
+}
+
+function loadModules() {
+	var moduleContainer = document.getElementById("modules");
+
+	// cloning single module template
+	var tmpl = document.getElementById("moduleTemplate");
+	var module = tmpl.content.cloneNode(true)
+
+	//inserting correct values for module
+	module.querySelector(".medName").innerHTML = "aminophen";
+	module.querySelector(".rate").innerHTML = "3.2" + "ml/hr";
+	module.querySelector(".dosage").innerHTML = "2.8" + "mcg/kg/hr";
+	module.querySelector(".status").innerHTML = "no alerts";
+	module.querySelector(".vtbi").innerHTML = "83.1" + "ml";
+
+	moduleContainer.appendChild(module);
 }
